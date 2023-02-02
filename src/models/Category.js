ebb35653343edd8d5879792279321e6169735c51
Thiago Reis Nascimento <trnasci@gmail.com>
2023-02-02 13:19:46 -0300

    Resolução do requisito 07

---
+++ src/models/Category.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        name: {
+          type: DataTypes.STRING
+        },
+      
+  },
+  {
+   underscored: true,
+   timestamps: false,
+   tableName: 'categories',
+  });
+
+  return Category; 
+}   
